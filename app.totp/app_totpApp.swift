c2c8d1a063946dc888edd61c5e39c2ae66783cdb
danish khan <danishdev64@gmail.com>
2025-06-17 17:26:42 +0530

    Initial Commit

---
+++ app.totp/app_totpApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_totpApp.swift
+//  app.totp
+//
+//  Created by PropertyShare on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct app_totpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
